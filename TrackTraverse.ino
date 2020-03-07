657a3e21006796a156bb3df463e2348013c9b467
wobey@uncc.edu <wobey@uncc.edu>
2020-03-07 09:17:11 -0500

    Test

--- TrackTraverse.ino
+++ TrackTraverse.ino
@@ -1,29 +1,29 @@
 #include <RedBot.h>
 #include <RedBotSoftwareSerial.h>
 
-
-RedBotMotors Motor; // declare motor object 
+ // Hello World :)
+RedBotMotors Motor; // declare motor object
 
 #define LeftMotorSpeed (50) // Set Left Motor to  roughly Half Speed
 #define RightMotorSpeed (50) // Set Right Motor to roughly Half Speed
 
 /*
- * Try 90 Later when actually displaying 
+ * Try 90 Later when actually displaying
 */
 
-void setup() 
+void setup()
 {
   // put your setup code here, to run once:
 
 }
 
-void loop() 
+void loop()
 {
   // put your main code here, to run repeatedly:
-  
-  RedBotSensor RightSensor = RedBotSensor(A7); // RedBot Sensor Object is Assigned to Right Sensor 
+
+  RedBotSensor RightSensor = RedBotSensor(A7); // RedBot Sensor Object is Assigned to Right Sensor
   RedBotSensor LeftSensor = RedBotSensor(A3); // Redbot Sensor Object is Assigned to Left Sensor
-  
+
   if ((LeftSensor.read() > 850) && (RightSensor.read() < 850))
   {
     // Drive Forward
@@ -35,14 +35,14 @@ void loop()
     // Left Wheel Turns off Right Wheel turns on
     // Turn Left
     Motor.rightMotor(RightMotorSpeed);
-    Motor.leftMotor(0); 
+    Motor.leftMotor(0);
   }
   else if((LeftSensor.read() > 850) && (RightSensor.read()))
   {
     // Left Wheel Turns On Right Wheel Turns on
-    // Turn Left 
+    // Turn Left
    Motor.rightMotor(0);
-   Motor.leftMotor(-LeftMotorSpeed);  
+   Motor.leftMotor(-LeftMotorSpeed);
   }
-  delay (10); 
+  delay (10);
 }
